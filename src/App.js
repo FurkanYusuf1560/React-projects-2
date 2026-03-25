5750dd43a1b84cadc8bf35543f44ff6021d6acea
FurkanYusuf1560 <furkanyusufcevik@gmail.com>
2026-03-25 09:39:25 +0300

    new projects

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import BMICalculator from './components/12. bmi-calculator';
 import ButtonRippleEffect from './components/13. button-ripple-effect';
 import DragAndDropFeature from './components/14. drag-and-drop';
 import FormValidation from './components/15. simple-form-validation';
+import FileUpload from './components/16. file-upload';
+import Quiz from './components/17. quiz-app';
+import NestedComments from './components/18. nested-comments';
 import DigitalClock from './components/2.digital-clock';
 import CountdownTimerTest from './components/3. countdown-timer/test';
 import StepProgressBarTest from './components/4. step-progress-bar/test';
@@ -32,8 +35,11 @@ function App() {
       <ProgressBar/>
       <BMICalculator/>
       <ButtonRippleEffect/> */}
-      <DragAndDropFeature/>
-      <FormValidation/>
+      {/* <DragAndDropFeature/>
+      <FormValidation/> */}
+      {/* <FileUpload/> */}
+      {/* <Quiz/> */}
+      <NestedComments/>
     </div>
   );
 }
